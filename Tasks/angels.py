7a986e4dc07bc5e4c4e25bca8be15c6a324ae722
Dolph <alnorob@outlook.com>
2021-09-11 17:45:25 +0100

    updated angels.py

--- Tasks/angels.py
+++ Tasks/angels.py
@@ -9,6 +9,30 @@ class AngelCounter(commands.Cog):
 
     def __init__(self,bot):
         self.bot = bot
+        self.update_angel_counters.start()
+
+    def cog_unload(self):
+
+        self.update_angel_counters.stop()
+
+    @tasks.loop(minutes=5)
+    async def update_angel_counters(self):
+
+        counters = {
+
+            "Global": 862279824461529098,
+            "Today": 880598351224143892,
+            "Skill210": 862647658271473664
+
+        }
+
+        for i in counters.items():
+
+            channel = i[1]
+
+            channel = self.bot.get_channel(channel)
+
+            print(channel)
 
     @commands.Cog.listener(name="on_message")
     async def increment_angels(self,ctx):
