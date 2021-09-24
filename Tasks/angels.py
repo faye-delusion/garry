1e3181ea59b80321a1214b894168d30504ccffd6
Dolph <alnorob@outlook.com>
2021-09-24 17:27:05 +0100

    Added angels command + command to add words to counter

--- Tasks/angels.py
+++ Tasks/angels.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import re
 import json
 import datetime
+import random
 
 from Functions import Angels
 from Functions import meta
@@ -94,6 +95,90 @@ class AngelCounter(commands.Cog):
             await Angels.increment_angels(ctx, kills)
 
             await meta.log(f"{ctx.author} killed an angel. They have killed {await Angels.get_angel_count(ctx)} so far.")
+
+
+    @commands.command(
+
+        name="add_angel_word",
+        aliases=["angelword"]
+
+    )
+    @commands.has_any_role(817475690558521346) # Student Council role
+    async def add_angel_word(self, ctx, * , word: str):
+
+        with open("badwords.json", "r") as f:
+
+            file = json.load(f)
+
+            file["badwords"].append(word)
+
+        with open("badwords.json", "w") as f:
+            
+            json.dump(file, f, indent=4)
+
+        await ctx.reply(f"Added `{word}` to list of angel killing words.")
+
+        await meta.log(f"Added word {word} to angel kill list.")
+
+    @commands.command(
+
+        name="angel_count",
+        aliases=["angel", "angels", "angelskilled"]
+
+    )
+    async def angel_count(self,ctx, user: discord.Member = None):
+
+        user = user or ctx.author
+
+        with open("users.json", "r") as f:
+
+            file = json.load(f)
+
+        if not str(user.id) in file:
+
+            file[str(user.id)] = {
+
+                "xp": 0,
+                "level": 1,
+                "prestige": 0,
+                "angels": 0
+            }
+
+        kills = file[str(user.id)]["angels"]
+
+        if kills <= 0:
+
+            word = random.choice([
+
+                "You are perfect.",
+                "Phenomenal.",
+                "What a perfect soul.",
+                "Keep it up!",
+                "Your path to heaven is looking clear!"
+
+            ])
+
+        else:
+
+            word = random.choice( [
+
+                "You are a disgrace.",
+                "How could you?",
+                "I thought I saw some potential in you.",
+                "Sinner!",
+                "Disgusting.",
+                "Vile, vile creature.",
+                "Begone! Leave!",
+                "Stay out of my sight."
+
+            ])
+
+        await ctx.send(embed=discord.Embed(
+
+            description=f"{user.mention} has killed {kills} angels. {word}",
+            colour=discord.Colour.random()
+
+        ))
 
 def setup(bot):
 
